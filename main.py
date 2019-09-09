2d45e9a71fe9a6dba314c1e8267ce3e9ed03f48c
Andrew Christensen <andrewpchristensen701@gmail.com>
2019-09-09 08:52:08 -0400

    Finalized

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 def add(x,y):
-  # your code here
-  return
+  return x+y
 
 def multiply(x,y):
-  # your code here
-  return
+  mul=0
+  for i in range(0,1):
+    mul=add(mul,y)
+  return mul
 
 def power(x,n):
-  # your code here
+ pwr = 1
+ for i in range(1,n):
+  multiply(pwr,x)
   return
 
 def factorial(x):
-  # your code here
-  return
+  fct = x
+  for i in range(2,x):
+    fct=multiply(fct,x-1)
+  return fct
 
 def fibonacci(n):
-  # your code here
-  return
+  fib=0
+  ona = 0
+  cci =1
+  if n == 0:
+    return "Error"
+  elif n == 1:
+    return 0
+  elif n == 2:
+    return 1
+  else:
+    for k in range(0,n):
+      fib = add(ona, cci);
+      ona = cci
+      cci = fib
+  return fib
